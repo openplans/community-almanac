95b5cafe71ab9e9b3833d4d7fc8d471505bcb68b
Douglas Mayle <douglas@mayle.org>
2009-05-07 10:26:16 -0400

    Add copyright header and license info to all of the existing files...

--- communityalmanac/model/__init__.py
+++ communityalmanac/model/__init__.py
@@ -1,3 +1,22 @@
+# Community Almanac - A place for your stories.
+# Copyright (C) 2009  Douglas Mayle, Robert Marianski,
+# Andy Cochran, Chris Patterson
+
+# This file is part of Community Almanac.
+
+# Community Almanac is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# Community Almanac is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Community Almanac.  If not, see <http://www.gnu.org/licenses/>.
+
 """The application's model objects"""
 import sqlalchemy as sa
 from sqlalchemy import orm
