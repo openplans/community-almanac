a198b19ff4c717346f3abea21d6a38d89fbbcd18
Robert Marianski <rob@marianski.com>
2009-05-11 16:03:32 -0400

    test for page create form
    --HG--
    extra : rebase_source : 5da5d08dbd582b6a6f817e63a1f37f8f1c1be1d7

--- communityalmanac/tests/functional/test_page.py
+++ communityalmanac/tests/functional/test_page.py
@@ -1,8 +1,15 @@
 from communityalmanac.tests import *
+from communityalmanac.model import Almanac
+from communityalmanac.model import meta
 
 class TestPageController(TestController):
 
+    def create_almanac(self, name, slug):
+        a = Almanac(name, slug)
+        meta.Session.save(a)
+        meta.Session.commit()
+        return a
+
     def test_create(self):
-        pass
-        #response = self.app.get(url(controller='page', action='create'))
-        # Test response...
+        almanac = self.create_almanac(u'my almanac for page', 'a4p')
+        response = self.app.get(url('page_create', almanac=almanac), status=200)
