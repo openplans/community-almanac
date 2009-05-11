78ffe519383fef353d3c46c3f7a14c3cf6d89a49
Robert Marianski <rob@marianski.com>
2009-05-11 13:03:05 -0400

    add stub for model tests with updated ini
    --HG--
    extra : rebase_source : f9d6ce8d415ab7a24dcd5a76df4925edad9bfded

--- communityalmanac/tests/test_models.py
+++ communityalmanac/tests/test_models.py
@@ -17,3 +17,21 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Community Almanac.  If not, see <http://www.gnu.org/licenses/>.
 
+from unittest import TestCase
+from communityalmanac import model
+
+class TestModel(TestCase):
+    """
+    We want the database to be created from scratch before each test and dropped after
+    each test (thus making them unit tests).
+    """
+    def setUp(self):
+        #model.resync()
+        #model.meta.create_all()
+        pass
+    def tearDown(self):
+        #model.meta.drop_all()
+        pass
+
+    def test_first(self):
+        self.failUnless(True)
