fcc59586401697d747f1276f40294306686f2c3d
Robert Marianski <rob@marianski.com>
2009-05-11 15:41:35 -0400

    simplify tests for almanac and page save
    --HG--
    extra : rebase_source : 7cd547a473dd7a370705b59298262e867e36573f

--- communityalmanac/tests/test_models.py
+++ communityalmanac/tests/test_models.py
@@ -17,31 +17,28 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Community Almanac.  If not, see <http://www.gnu.org/licenses/>.
 
-from unittest import TestCase
 from communityalmanac.model import Almanac
 from communityalmanac.model import Page
 from communityalmanac.model import meta
 
-class TestModel(TestCase):
-    """
-    We want the database to be created from scratch before each test and dropped after
-    each test (thus making them unit tests).
-    """
-    def setUp(self):
-        meta.metadata.drop_all(meta.engine)
-        meta.Session.remove()
-        meta.metadata.create_all(meta.engine)
+def test_almanac_save():
+    a = Almanac(u'my first almanac', 'a1')
+    meta.Session.save(a)
+    meta.Session.commit()
+    assert a.id, "did not save almanac to database"
+    query = meta.Session.query(Almanac)
+    queried = query.one()
+    assert a.name == queried.name
+    assert a.slug == queried.slug
 
-    def test_first(self):
-        a = Almanac(u'my first almanac', 'first')
-        meta.Session.save(a)
-        meta.Session.commit()
+def test_page_save():
+    a = Almanac(u'almanac two', 'a2')
+    meta.Session.save(a)
+    p = Page(u'page one', 'p1')
+    a.pages.append(p)
+    meta.Session.save(p)
+    meta.Session.commit()
 
-    def test_second(self):
-        self.failUnless(meta.Session.query(Almanac).first() is None)
-        a = Almanac(u'my first almanac', 'first')
-        p = Page(u'my first page', 'page')
-        p.almanac_id = a.id
-        meta.Session.save(a)
-        meta.Session.save(p)
-        meta.Session.commit()
+    assert a.id
+    assert p.id
+    assert p.almanac_id == a.id
