96a5ab69661d6cc6325d30e5ee1a71eff701da9f
Robert Marianski <rob@marianski.com>
2009-06-08 16:29:07 -0400

    intermittent commit to illustrate bug

--- communityalmanac/controllers/media.py
+++ communityalmanac/controllers/media.py
@@ -314,7 +314,7 @@ class MediaController(BaseController):
         meta.Session.commit()
 
         c.editable = True
-        return render('/media/image/item.mako')
+        return simplejson.dumps(html=render('/media/image/item.mako'))
 
     @dispatch_on(POST='_do_new_form_existing_image')
     @jsonify
