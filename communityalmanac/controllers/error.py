2ba2524151a4a6abbedd6246b47b9889dec9933a
Robert Marianski <rob@marianski.com>
2009-06-10 14:01:06 -0400

    add stubs for 404 and 500 error pages

--- communityalmanac/controllers/error.py
+++ communityalmanac/controllers/error.py
@@ -23,6 +23,7 @@ from paste.urlparser import PkgResourcesParser
 from pylons import request
 from pylons.controllers.util import forward
 from pylons.middleware import error_document_template
+from pylons.templating import render_mako as render
 from webhelpers.html.builder import literal
 
 from communityalmanac.lib.base import BaseController
@@ -43,6 +44,12 @@ class ErrorController(BaseController):
         """Render the error document"""
         resp = request.environ.get('pylons.original_response')
         content = literal(resp.body) or cgi.escape(request.GET.get('message', ''))
+        code = resp.status_int
+        if code == 404:
+            return render('/404.mako')
+        elif code == 500:
+            return render('/500.mako')
+
         page = error_document_template % \
             dict(prefix=request.environ.get('SCRIPT_NAME', ''),
                  code=cgi.escape(request.GET.get('code', str(resp.status_int))),
