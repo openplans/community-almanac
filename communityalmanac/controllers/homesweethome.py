7d5a49e9b60625c6b3e81bfb9a73fccdd3637c8f
Douglas Mayle <douglas@mayle.org>
2009-06-18 00:16:03 +0200

    Someone forgot to commit this...

--- communityalmanac/controllers/homesweethome.py
+++ communityalmanac/controllers/homesweethome.py
@@ -6,6 +6,7 @@ from pylons.controllers.util import abort, redirect_to
 from pylons.decorators import jsonify
 
 from communityalmanac.lib.base import BaseController, render
+from communityalmanac.lib import helpers as h
 from communityalmanac.model import Almanac
 from communityalmanac.model import Page
 from communityalmanac.model import meta
