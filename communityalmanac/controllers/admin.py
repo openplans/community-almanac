e8edb6f9439609ff9809ab4c3d4a15923658b8f1
Robert Marianski <rob@marianski.com>
2009-06-24 13:50:13 -0400

    add basic implementation of geo renderer
    --HG--
    extra : rebase_source : c531d0b6a678d6610407428c7298fbbd96dfafae

--- communityalmanac/controllers/admin.py
+++ communityalmanac/controllers/admin.py
@@ -18,3 +18,32 @@ class AdminController(BaseController):
 
 AdminController = FormAlchemyAdminController(AdminController)
 AdminController = ControllerProtector(has_permission('manage'))(AdminController)
+
+
+from formalchemy.fields import FieldRenderer
+from formalchemy.forms import FieldSet
+from formalchemy import helpers as h
+from shapely import wkb
+from shapely.geometry.geo import asShape
+import simplejson
+
+class GeoRenderer(FieldRenderer):
+    def render(self, **kwargs):
+        shape = self.field.model_value
+        geo_interface = shape.__geo_interface__
+        json = simplejson.dumps(geo_interface)
+        return h.text_field(self.name, value=json)
+
+    def _deserialize(self, data):
+        json = data
+        shape = simplejson.loads(json)
+        location = wkb.loads(asShape(shape).to_wkb())
+        # uncommenting this causes a transform error
+        #location.srid = 4326
+        return location
+
+
+from sqlalchemy import types as sqltypes
+
+FieldSet.default_renderers[model.sqlgeotypes.POINT] = GeoRenderer
+FieldSet.default_renderers[sqltypes.NullType] = GeoRenderer
