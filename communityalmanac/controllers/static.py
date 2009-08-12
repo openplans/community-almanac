1dc533a34938ef74b9befa85c0ed547f414bbce9
Douglas Mayle <douglas@mayle.org>
2009-08-12 17:17:59 +0200

    Fix controller name...

--- communityalmanac/controllers/static.py
+++ communityalmanac/controllers/static.py
@@ -2,7 +2,7 @@ from communityalmanac.lib.base import BaseController, render
 from communityalmanac.model import Page
 from pylons import tmpl_context as c
 
-class AboutController(BaseController):
+class StaticController(BaseController):
     def about(self):
         c.no_maps = True
         c.latest_pages = Page.latest()
